1b0525d1bacc6072b8ea1e9996479bbdd8caf874
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-05-19 20:15:39 -0400

    updated the model name to product as per the sba

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const bookSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema({
 name: { type: String , required: true },
 description: { type: String , required: true },
 price: { type: Number , required: true },
@@ -7,6 +7,6 @@ category: { type: String, required: true },
 inStock: { Boolean: true },
 });
 //creating a Mongoose schema called bookSchema that defines the structure and validation rules for book data in the database.”
-const Book = mongoose.model("Book", bookSchema);
+const Product = mongoose.model("Product", productSchema);
 //creating a Mongoose model called Book using the bookSchema, which will be used to interact with the books collection in MongoDB.”
 module.exports = Book;
---
+++ routes/productRoutes.js
@@ -0,0 +1,6 @@
+//POST /api/products (Create a Product)
+const express = require('express');
+
+const router = express.Router();
+
+const 
