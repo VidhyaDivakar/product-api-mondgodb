493f6faa194edb17ffbef18559f1b15f97430134
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-05-19 20:06:14 -0400

    db connected

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,11 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const connectDB = async () => {
-//     try{
-// await mongoose.connect(process.env.DATABASE_URI);
-// console.log("Mongo DB Connected");
-//     }catch(error){
-//         console.log("Mongo Error:", error);
-//     }
-// };
-//     module.exports = connectDB;
+const connectDB = async () => {
+    try{
+await mongoose.connect(process.env.DATABASE_URI);
+console.log("Mongo DB Connected");
+    }catch(error){
+        console.log("Mongo Error:", error);
+    }
+};
+    module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const dns = require("dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
+const express = require("express");
+const { default: mongoose } = require("mongoose");
 
+const app = express();
 const connectDB = require("./db/connection");
 require("dotenv").config();
 connectDB();
 const PORT = 3005;
 
 
-
+app.get("/", (req,  res) => {
+    console.log("App Connected");
+    res.send("Server is running");
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
