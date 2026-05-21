544753360db779f5e832bd08ddb2cdf149428db2
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-05-21 16:30:00 -0400

    addded the other methods, advanced query

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -51,3 +51,59 @@ router.put("/:id", async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
+
+//  (Delete a Product)  DELETE /api/products/:id
+router.delete("/:id", async (req, res) => {
+    try {
+        const product = await Product.findByIdAndDelete(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        res.json({ message: "Product deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// (Read All Products with Advanced Querying) GET /api/products
+router.get("/", async (req, res) => {
+    try {
+        const { category, minPrice, maxPrice, sortBy, page = 1, limit = 10 } = req.query;
+
+        let query = {};
+
+        // filtering
+        if (category) query.category = category;
+
+        if (minPrice || maxPrice) {
+            query.price = {};
+            if (minPrice) query.price.$gte = Number(minPrice);
+            if (maxPrice) query.price.$lte = Number(maxPrice);
+        }
+
+        let dbQuery = Product.find(query);
+
+        // sorting
+        if (sortBy === "price_asc") {
+            dbQuery = dbQuery.sort({ price: 1 });
+        } else if (sortBy === "price_desc") {
+            dbQuery = dbQuery.sort({ price: -1 });
+        }
+
+        // pagination
+        const skip = (page - 1) * limit;
+
+        dbQuery = dbQuery.skip(skip).limit(Number(limit));
+
+        const products = await dbQuery;
+
+        res.json(products);
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+module.exports = router;
+ 
